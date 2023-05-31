1bbe46adc3a2c68e53e130267820a9574e04bc62
Zeynep Özdemir Açıkgöz <zeynmir@gmail.com>
2023-05-31 14:35:43 +0300

    setting product TableViewCell and it's CollectionView

--- Bebiller/Controllers/ProductViewController.swift
+++ Bebiller/Controllers/ProductViewController.swift
@@ -8,13 +8,57 @@
 import UIKit
 
 class ProductViewController: UIViewController {
+    private let productFeedTable: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+        return table
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        view.addSubview(productFeedTable)
+        
+        productFeedTable.delegate = self
+        productFeedTable.dataSource = self
+        
+        productFeedTable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 300))
     }
     
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        productFeedTable.frame = view.bounds
+    }
     
+}
+
+extension ProductViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.textLabel?.text = "Hello world"
+//        cell.backgroundColor = .yellow
+//        return cell
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
 }
