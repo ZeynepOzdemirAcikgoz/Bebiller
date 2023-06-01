db45ca463f56d44f28f91a90b8f5864d3e39fe34
Zeynep Özdemir Açıkgöz <zeynmir@gmail.com>
2023-06-01 13:34:35 +0300

    navigation bar is set.

--- Bebiller/Controllers/HomeViewController.swift
+++ Bebiller/Controllers/HomeViewController.swift
@@ -16,12 +16,46 @@ class HomeViewController: UIViewController {
 
         view.backgroundColor = .systemBackground
         
-        
+        configureNavbar()
        
     }
     
    
-
+    private func configureNavbar(){
+        
+//        var image = UIImage(named: "logo")
+//        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+//
+        
+        let navController = navigationController!
+        var image = UIImage(named: "logo")
+        image = image?.withRenderingMode(.alwaysOriginal)
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        
+        let oiButton = UIButton(type: .system)
+        oiButton.setImage(UIImage(named: "logo")?.withRenderingMode(.alwaysOriginal), for: .normal)
+       // oiButton.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        oiButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        let bannerHeight = navController.navigationBar.frame.size.height
+        let bannerWidth = bannerHeight + 20
+        
+        
+        let bannerX = bannerWidth / 2 - (image?.size.width)!
+        let bannerY = bannerHeight / 2
+        oiButton.widthAnchor.constraint(equalToConstant: bannerWidth).isActive = true
+        oiButton.heightAnchor.constraint(equalToConstant: bannerHeight).isActive = true
+        
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: oiButton)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defaultOffset
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
+    }
     
 
 }
--- Bebiller/Controllers/ProductViewController.swift
+++ Bebiller/Controllers/ProductViewController.swift
@@ -27,6 +27,38 @@ class ProductViewController: UIViewController {
         //productFeedTable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 300))
         let headerView = HeroHeadeUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width , height: 300))
         productFeedTable.tableHeaderView = headerView
+        
+        configureNavbar()
+    }
+    
+    private func configureNavbar(){
+        
+//        var image = UIImage(named: "logo")
+//        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+//
+        
+        let navController = navigationController!
+        var image = UIImage(named: "logo")
+        image = image?.withRenderingMode(.alwaysOriginal)
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        
+        let oiButton = UIButton(type: .system)
+        oiButton.setImage(UIImage(named: "logo")?.withRenderingMode(.alwaysOriginal), for: .normal)
+       // oiButton.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        oiButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        let bannerHeight = navController.navigationBar.frame.size.height
+        let bannerWidth = bannerHeight + 20
+        
+        
+        let bannerX = bannerWidth / 2 - (image?.size.width)!
+        let bannerY = bannerHeight / 2
+        oiButton.widthAnchor.constraint(equalToConstant: bannerWidth).isActive = true
+        oiButton.heightAnchor.constraint(equalToConstant: bannerHeight).isActive = true
+        
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: oiButton)
     }
     
     override func viewDidLayoutSubviews() {
@@ -62,5 +94,11 @@ extension ProductViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 40
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defaultOffset
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
     }
 }
